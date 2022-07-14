c24da5f26551120785919d2124d5cbc99cf2619b
Piotr Słomski <piotrslomski73@gmaail.com>
2022-07-14 07:43:22 +0200

    Delete debug dir

--- test/sdl/TestWindow.cpp
+++ test/sdl/TestWindow.cpp
@@ -4,7 +4,7 @@
 
 TEST(Window, shouldCreate)
 {
-    sdl::Window* window = sdl::Window::create(nullptr, 100, 100);
+    sdl::Window *window = sdl::Window::create(nullptr, 100, 100);
     ASSERT_TRUE(window != nullptr);
     sdl::Window::destroy(window);
     ASSERT_TRUE(window == nullptr);
